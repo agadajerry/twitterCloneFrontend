57eff630533d29319a2bc061e3a3568a684232a5
okarachidera <okara.chidera@gmail.com>
2022-03-14 21:59:33 +0100

    merge: update tweet component

--- src/Components/CreateTweet.tsx
+++ src/Components/CreateTweet.tsx
@@ -1,109 +1,104 @@
-import axios from 'axios';
-import { motion } from 'framer-motion';
-import  { useContext, useState } from 'react'
-import { Circles } from 'react-loader-spinner';
-import Swal from 'sweetalert2';
-import { AVATAR } from '../constants/contants';
+import axios from "axios";
+import { motion } from "framer-motion";
+import { useContext, useState } from "react";
+import { Circles } from "react-loader-spinner";
+import Swal from "sweetalert2";
+import { AVATAR, BASE_URL } from "../constants/contants";
 // import { BASE_URL } from '../constants/contants';
-import { UserContext } from '../hooks/useContext';
+import { UserContext } from "../hooks/useContext";
 import styles from "../styles/Tweeting_style/TweetController.module.css";
 
 function CreateTweet() {
-
   const { user, setUser }: any = useContext(UserContext);
 
-  
   // const userToken:any = useContext(UserContext)
-  const token = user.token
-
+  const token = user.token;
 
   // const url = `${BASE_URL}api/trends`
   // const uri = `${BASE_URL}api/follow/suggest/?pageNo=2&pageSize=5`
 
+  //   useEffect(() => {
+  //     axios
+  //       .get(url, {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       })
+  //       .then((res) => {
+  //         console.log(res.data)
+  //         setTrends(res.data.data.trending);
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
 
-//   useEffect(() => {
-//     axios
-//       .get(url, {
-//         headers: {
-//           Authorization: `Bearer ${token}`,
-//         },
-//       })
-//       .then((res) => {
-//         console.log(res.data)
-//         setTrends(res.data.data.trending);
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
+  //       axios
+  //         .get(uri, {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       })
+  //       .then((res)=>{
+  //         console.log(res.data)
+  //         setFollow(res.data.data.suggestedConnection)
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   }, []);
 
-//       axios
-//         .get(uri, {
-//         headers: {
-//           Authorization: `Bearer ${token}`,
-//         },
-//       })
-//       .then((res)=>{
-//         console.log(res.data)
-//         setFollow(res.data.data.suggestedConnection)
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   }, []);
+  const [newheight, setNewHeight] = useState("22px");
+  const [modal, setModal] = useState(false);
+  const [whoCanReplay, setWhoCanReplay] = useState("Everyone");
+  const [imageUrl, setImageUrl] = useState("");
+  const [imageHeight, setImageHeight] = useState("");
+  const [imageWidth, setImageWidth] = useState("");
+  const [exitImage, setExitImage] = useState("");
 
-    const [newheight, setNewHeight] = useState("22px");
-    const [modal, setModal] = useState(false);
-    const [whoCanReplay, setWhoCanReplay] = useState("Everyone");
-    const [imageUrl, setImageUrl] = useState("");
-    const [imageHeight, setImageHeight] = useState("");
-    const [imageWidth, setImageWidth] = useState("");
-    const [exitImage, setExitImage] = useState("");
-  
-    const [enteredText, setEnteredText] = useState("");
-    const [enteredImg, setEnteredImg] = useState<null | File>(null);
-    const [loading, setLoading] = useState(false);
+  const [enteredText, setEnteredText] = useState("");
+  const [enteredImg, setEnteredImg] = useState<null | File>(null);
+  const [loading, setLoading] = useState(false);
 
+  const formData = new FormData();
+  const addTextHandler = async (event: any) => {
+    formData.append("messageBody", enteredText);
+    formData.append("tweetImage", enteredImg as File);
+    formData.append("whoCanReply", whoCanReplay);
 
+    try {
+      setLoading(true);
+      const data = await axios.post(`${BASE_URL}tweeting`, formData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Accept-Language": "en-US,en;q=0.8",
+          "Content-Type": `multipart/form-data`,
+        },
+      });
 
+      // Swal.fire("Hey Twitee!", "Tweet posted!", "success");
+      Swal.fire({
+        icon: "success",
+        title: "Created tweet successfully",
+        showConfirmButton: false,
+        timer: 2500,
+      });
 
+      console.log(data);
+      setLoading(false);
+      setEnteredText("");
+      removeImage();
+      setEnteredImg(null);
+    } catch (e: any) {
+      console.log(e.response.data);
+      setLoading(false);
+    }
 
-    const formData = new FormData();
-    const addTextHandler = async (event: any) => {
-      formData.append("messageBody", enteredText);
-      formData.append("tweetImage", enteredImg as File);
-      formData.append("whoCanReply", whoCanReplay);
-  
-      try {
-        setLoading(true);
-        const data = await axios.post(
-          "https://tweetaclone.herokuapp.com/tweeting",
-          formData,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-              "Accept-Language": "en-US,en;q=0.8",
-              "Content-Type": `multipart/form-data`,
-            },
-          }
-        );
-  
-        Swal.fire("Hey Twitee!", "Tweet posted!", "success");
-        console.log(data);
-        setLoading(false);
-        setEnteredText("");
-        removeImage();
-        setEnteredImg(null);
-      } catch (e: any) {
-        console.log(e.response.data);
-        setLoading(false);
-      }
-  
-      // for (var pair of formData.entries()) {
-      //   console.log(pair[0] + ", " + pair[1]);
-      // }
-    };
+    // for (var pair of formData.entries()) {
+    //   console.log(pair[0] + ", " + pair[1]);
+    // }
+  };
 
-     //get text of who can replay
+  //get text of who can replay
   function getText(e: any) {
     setWhoCanReplay(e.currentTarget.textContent);
     setModal(false);
@@ -128,7 +123,7 @@ function CreateTweet() {
   function increaseHeight(e: any) {
     let changeHeight = e.target.scrollHeight;
     setEnteredText(e.target.value);
-    console.log(enteredText)
+    console.log(enteredText);
 
     setNewHeight(changeHeight);
 
@@ -137,122 +132,105 @@ function CreateTweet() {
     }
   }
 
-  
   return (
     <>
-    <div className={styles["tweet-heading"]}>
-            <h3>Tweet something</h3>
-            <div className={styles.underline}></div>
-            <div className={styles["write-message"]}>
-              <img
-                src={user.user.profilePic || AVATAR}
-                className={styles["user-image"]}
-                alt="user"
+      <div className={styles["tweet-heading"]}>
+        <h3>Tweet something</h3>
+        <div className={styles.underline}></div>
+        <div className={styles["write-message"]}>
+          <img src={user.user.profilePic || AVATAR} className={styles["user-image"]} alt="user" />
+          <motion.textarea
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1, duration: 2 }}
+            placeholder="What's happening?"
+            onChange={(e) => increaseHeight(e)}
+            style={{ height: newheight }}
+            value={enteredText}
+          ></motion.textarea>
+        </div>
+
+        <div className={styles["set-media-image"]}>
+          <img
+            alt=""
+            src={imageUrl ? imageUrl : ""}
+            style={{
+              width: imageWidth,
+              height: imageHeight,
+              borderRadius: "4px",
+            }}
+          />
+          <span onClick={removeImage}>
+            <img src={exitImage} alt="" />
+          </span>
+        </div>
+
+        <div className={styles["select-media-container"]}>
+          <div className={styles["select-media"]}>
+            {/* select media */}
+
+            <p style={{ display: "none" }}>
+              <input
+                type="file"
+                id="file"
+                accept="image/*"
+                name="image"
+                onChange={(e) => getImage(e)}
               />
-              <motion.textarea
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 1, duration: 2 }}
-                placeholder="What's happening?"
-                onChange={(e) => increaseHeight(e)}
-                style={{ height: newheight }}
-                value={enteredText}
-              ></motion.textarea>
-            </div>
+            </p>
 
-            <div className={styles["set-media-image"]}>
-              <img
-                alt=""
-                src={imageUrl ? imageUrl : ""}
-                style={{
-                  width: imageWidth,
-                  height: imageHeight,
-                  borderRadius: "4px",
-                }}
-              />
-              <span onClick={removeImage}>
-                <img src={exitImage} alt="" />
-              </span>
-            </div>
+            <p className={styles["select-image"]}>
+              <label htmlFor="file">
+                <img src="https://img.icons8.com/material-outlined/48/2F80ED/image.png" alt="img" />
+              </label>
+            </p>
 
-            <div className={styles["select-media-container"]}>
-              <div className={styles["select-media"]}>
-                {/* select media */}
-
-                <p style={{ display: "none" }}>
-                  <input
-                    type="file"
-                    id="file"
-                    accept="image/*"
-                    name="image"
-                    onChange={(e) => getImage(e)}
-                  />
-                </p>
-
-                <p className={styles["select-image"]}>
-                  <label htmlFor="file">
-                    <img
-                      src="https://img.icons8.com/material-outlined/48/2F80ED/image.png"
-                      alt="img"
-                    />
-                  </label>
-                </p>
-
-                <div onClick={() => setModal(!modal)}>
-                  <img
-                    className={styles["media-image"]}
-                    src="https://img.icons8.com/material-rounded/48/2F80ED/globe--v1.png"
-                    alt="img"
-                  />
-                  <h3>{whoCanReplay}</h3>
-                </div>
-              </div>
-
-              <button
-                type="submit"
-                onClick={addTextHandler}
-              >
-                {loading === true ? (
-                  <Circles
-                    color="#FFFFFF"
-                    height={20}
-                    width={20}
-                    wrapperStyle={{ justifyContent: "center" }}
-                  />
-                ) : (
-                  "Tweet"
-                )}
-              </button>
-            </div>
-          </div>
-          <div
-            className={
-              modal
-                ? styles["replierContainer"]
-                : styles["hideReplierContainer"]
-            }
-          >
-            <h3>Who can reply?</h3>
-            <p>Choose who can reply</p>
-            <a href="#!" className={styles["image-n-text"]}>
+            <div onClick={() => setModal(!modal)}>
               <img
                 className={styles["media-image"]}
-                src="https://img.icons8.com/material-rounded/28/000000/globe--v1.png"
+                src="https://img.icons8.com/material-rounded/48/2F80ED/globe--v1.png"
                 alt="img"
               />
-              <h3 onClick={(e) => getText(e)}>Everyone</h3>
-            </a>
-            <a href="#!"  className={styles["image-n-text"]}>
-              <img
-                className={styles["media-image"]}
-                src="https://img.icons8.com/ios-glyphs/30/000000/group-foreground-selected.png"
-                alt="img"
-              />
-              <h3 onClick={(e) => getText(e)}>People you follow</h3>
-            </a>
+              <h3>{whoCanReplay}</h3>
+            </div>
           </div>
+
+          <button type="submit" onClick={addTextHandler}>
+            {loading === true ? (
+              <Circles
+                color="#FFFFFF"
+                height={20}
+                width={20}
+                wrapperStyle={{ justifyContent: "center" }}
+              />
+            ) : (
+              "Tweet"
+            )}
+          </button>
+        </div>
+      </div>
+      <div className={modal ? styles["replierContainer"] : styles["hideReplierContainer"]}>
+        <h3>Who can reply?</h3>
+        <p>Choose who can reply</p>
+        <a href="#!" className={styles["image-n-text"]}>
+          <img
+            className={styles["media-image"]}
+            src="https://img.icons8.com/material-rounded/28/000000/globe--v1.png"
+            alt="img"
+          />
+          <h3 onClick={(e) => getText(e)}>Everyone</h3>
+        </a>
+        <a href="#!" className={styles["image-n-text"]}>
+          <img
+            className={styles["media-image"]}
+            src="https://img.icons8.com/ios-glyphs/30/000000/group-foreground-selected.png"
+            alt="img"
+          />
+          <h3 onClick={(e) => getText(e)}>People you follow</h3>
+        </a>
+      </div>
     </>
-  )
+  );
 }
 
-export default CreateTweet
+export default CreateTweet;
